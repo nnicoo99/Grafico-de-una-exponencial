4ab13ef6a1666cd4d42dc40706dabcf28de9c22a
nico <conchanicolas1@gmail.com>
2025-03-27 00:03:27 -0300

    te amo cris

---
+++ cris.py
@@ -0,0 +1,21 @@
+# Importamos las librerías necesarias
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Datos
+x = np.linspace(0, 10, 100)  # Generamos 100 puntos entre 0 y 10
+y = np.exp(x)  # Calculamos e^x para cada valor de x
+
+# Creamos el gráfico
+plt.plot(x, y, label='y = e^x', color='g')
+
+# Añadimos título y etiquetas
+plt.title('Gráfico Exponencial en Python')
+plt.xlabel('x')
+plt.ylabel('y = e^x')
+
+# Añadimos una leyenda
+plt.legend()
+
+# Mostramos el gráfico
+plt.show()
